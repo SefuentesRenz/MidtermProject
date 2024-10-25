3dd0d6b27cceb543e9190c344998c26bad9383d7
Renz Angelo T. Sefuentes <142190484+SefuentesRenz@users.noreply.github.com>
2024-10-25 11:21:35 +0800

    Create recipe_model2.dart

---
+++ lib/models/recipe_model2.dart
@@ -0,0 +1,8 @@
+class Recipe {
+  final String name;
+  final String description;
+  final String ingredients;
+  final String instructions;
+
+  Recipe(this.name, this.description, this.ingredients, this.instructions);
+}
